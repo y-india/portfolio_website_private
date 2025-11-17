5068a9cabb82eddcfdcaae7d693fa64c94eabb48
Yuvraj <y.india.main@gmail.com>
2025-11-17 21:44:34 +0530

    very little change

--- pages/1_👤_About_Me.py
+++ pages/1_👤_About_Me.py
@@ -87,7 +87,7 @@ with col2:
         ">
         <h2 style="font-weight:800;">About Me</h2>
 
-        Helloo, age is 17 ,I’m <b>Yuvraj Rana</b>, a beginner Python and Machine Learning developer from a 
+        Helloo ,I’m <b>Yuvraj Rana</b>, a beginner Python and Machine Learning developer from a 
         village near Ambala, Haryana. I’m a self-taught learner who enjoys exploring technology 
         and building things that make life easier.
 
